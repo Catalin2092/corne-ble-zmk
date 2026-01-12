28d4a1f7f5af8fab05b3779f78df43c1ed2316f9
Ionut-Catalin Mihalcea <ionut-catalin.mihalcea@renault.com>
2026-01-12 18:03:40 +0200

    call the led in led_control.c

--- config/led_control.c
+++ config/led_control.c
@@ -7,27 +7,31 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#define LED_NODE DT_NODELABEL(led_0)
+// Get GPIO device and pin directly from devicetree
+#define LED_GPIO_NODE DT_NODELABEL(pro_micro)
+#define LED_PIN 8
 
-#if DT_NODE_EXISTS(LED_NODE)
+#if DT_NODE_EXISTS(LED_GPIO_NODE)
 
-static const struct gpio_dt_spec led_gpio = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct device *gpio_dev;
 
 static int led_control_init(void) {
-    if (!gpio_is_ready_dt(&led_gpio)) {
+    gpio_dev = DEVICE_DT_GET(LED_GPIO_NODE);
+    
+    if (!device_is_ready(gpio_dev)) {
         LOG_ERR("LED GPIO device not ready");
         return -ENODEV;
     }
 
-    int ret = gpio_pin_configure_dt(&led_gpio, GPIO_OUTPUT_ACTIVE);
+    int ret = gpio_pin_configure(gpio_dev, LED_PIN, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         LOG_ERR("Failed to configure LED GPIO pin");
         return ret;
     }
 
     // Turn LED on at boot
-    gpio_pin_set_dt(&led_gpio, 1);
-    LOG_INF("LED control initialized, LED turned on");
+    gpio_pin_set(gpio_dev, LED_PIN, 1);
+    LOG_INF("LED control initialized on pin %d, LED turned on", LED_PIN);
 
     return 0;
 }
@@ -37,11 +41,11 @@ static int led_activity_listener(const zmk_event_t *eh) {
 
     if (state == ZMK_ACTIVITY_IDLE) {
         // Turn LED off when idle
-        gpio_pin_set_dt(&led_gpio, 0);
+        gpio_pin_set(gpio_dev, LED_PIN, 0);
         LOG_INF("Activity idle - LED turned off");
     } else if (state == ZMK_ACTIVITY_ACTIVE) {
         // Turn LED on when active
-        gpio_pin_set_dt(&led_gpio, 1);
+        gpio_pin_set(gpio_dev, LED_PIN, 1);
         LOG_INF("Activity active - LED turned on");
     }
 
